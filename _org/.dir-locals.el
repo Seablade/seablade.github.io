699eebb80cf8a668e4c1bc5377f2b4f26a387b2d
Thomas Vecchione <thomas.vecchione@atleechurch.org>
2013-11-27 23:58:26 -0500

    First round of updates to bring old content into new site

--- _org/.dir-locals.el
+++ _org/.dir-locals.el
@@ -1,6 +1,6 @@
 ((nil . ((org-publish-project-alist
 	      . (("org-notes"               ;Used to export .org file
-		   :base-directory "~/Sites/Github/org/"  ;directory holds .org files 
+		   :base-directory "~/Sites/Github/_org/"  ;directory holds .org files 
 		   :base-extension "org"     ;process .org file only    
 		   :publishing-directory "~/Sites/Github/"    ;export destination
 					;:publishing-directory "/ssh:user@server" ;export to server
@@ -8,7 +8,7 @@
 		   :publishing-function org-publish-org-to-html
 		   :headline-levels 4               ; Just the default for this project.
 		   :auto-preamble nil
-		   :auto-sitemap t                  ; Generate sitemap.org automagically...
+		   :auto-sitemap nil                  ; Generate sitemap.org automagically...
 		   :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
 		   :sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
 		   :export-creator-info nil    ; Disable the inclusion of "Created by Org" in the postamble.
@@ -16,13 +16,8 @@
 		   :auto-postamble nil         ; Disable auto postamble 
 		   :table-of-contents nil      ; Set this to "t" if you want a table of contents, set to "nil" disables TOC.
 		   :section-numbers nil        ; Set this to "t" if you want headings to have numbers.
-                   :html-preamble "    
-  <span id=\"header\"><h1>Leviathan-AVC</h1></span>
-  <hr>"
-		   :html-postamble "    
-<p class=\"postamble\">Last Updated %d.</p>
-<script type=\"text/javascript\"> $( \"body\" ).wrapInner(\"<div class='container'></div>\"); 
-                                  $( \"#header\" ).append( $(\".navbar\"))</script> "
+                   :html-preamble " "          ; Contents of preamble 
+		   :html-postamble " "         ; Contents of postamble
 		   :style-include-default nil  ;Disable the default css style
 		   )
 		  ("org-static"                ;Used to publish static files
@@ -32,6 +27,6 @@
 		   :recursive t
 		   :publishing-function org-publish-attachment
 		   )
-		  ("github-blog" :components ("org-notes" "org-static")) ;combine "org-static" and "org-static" into one function call
+		  ("github-blog" :components ("org-notes")) ; Combine into one function call, add "org-static" with a space if desired 
 		  )))
 ))
